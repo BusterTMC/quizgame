101a23b8505048b9dcff78441bd2cb93e842a093
BusterTMC <>
2024-02-01 14:46:38 +0000

    Made some chnges to teh qustions in the script file and added getElementbyid

--- script.js
+++ script.js
@@ -1,75 +1,62 @@
-const data = [
+var questions = [
     {
-        question: "Who painted the Mona Lisa??",
-        a: "Vincent van Gogh",
-        b: "Papblo Picasso",
-        c: "Leonardo da Vinci",
-        d: "Michelangelo",
-        correct: "c",
+        "question":"Who painted the Mona Lisa?",
+        "option":["Vincent van Gogh","Papblo Picasso","Leonardo da Vinci","Michelangelo"],
+        "answer":"Leonardo da Vinci"
     },
     {
-        question: "What is the capital of France?",
-        a: "Paris",
-        b: "Monaco",
-        c: "Marseille",
-        d: "Nice",
-        correct: "a",
+        "question":"What is the capital of France?",
+        "option":["Paris","Monaco","Marseille","Nice"],
+        "answer":"Paris"
     },
     {
-        question: "What is the tallest mountain in the world?",
-        a: "K2",
-        b: "Mount Everest",
-        c: "Makalu",
-        d: "Lhotse",
-        correct: "b",
+        "question":"What is the tallest mountain in the world?",
+        "option":["K2","Mount Everest","Makalu","Lhotse"],
+        "answer":"Mount Everest"
     },
     {
-        question: "What is the largest planet in our solar system?",
-        a: "Saturn",
-        b: "Uranus",
-        c: "Neptune",
-        d: "Jupiter",
-        correct: "d",
+        "question":"What is the largest planet in our solar system?",
+        "option":["Saturn","Uranus","Neptune","Jupiter"],
+        "answer":"Jupiter"
     },
     {
-        question: "Who wrote the play Romeo and Juliet?",
-        a: "J.R.R. Tolkien",
-        b: "Charles Dickens",
-        c: "Lev Tolstoj",
-        d: "William Shakespeare",
-        correct: "d",
+        "question":"Who wrote the play Romeo and Juliet?",
+        "option":["J.R.R. Tolkien","Charles Dickens","Lev Tolstoj","William Shakespeare"],
+        "answer":"William Shakespeare"
     },
     {
-        question: "What is the capital city of Australia?",
-        a: "Perth",
-        b: "Canberra",
-        c: "Melbourne",
-        d: "Sydney",
-        correct: "b",
+        "question":"What is the capital city of Australia?",
+        "option":["Perth","Canberra","Melbourne","Sydney"],
+        "answer":"Canberra"
     },
     {
-        question: "What is the primary language spoken in Brazil?",
-        a: "French",
-        b: "Spanish",
-        c: "Portuguese",
-        d: "English",
-        correct: "c"
+        "question":"What is the primary language spoken in Brazil?",
+        "option":["French","Spanish","Portuguese","English"],
+        "answer":"Portuguese"
     },
-    {
-        question: "Who is known as the inventor of the lightbulb?",
-        a: "Thomas Edison",
-        b: "Nikola Tesla",
-        c: "Alexander Graham Bell",
-        d: "Albert Einstein",
-        correct: "a",
+     {
+        "question":"Who is known as the inventor of the lightbulb?",
+        "option":["Thomas Edison","Nikola Tesla","Alexander Graham Bell","Albert Einstein"],
+        "answer":"Thomas Edison"
     },
-    {
-        question: "What is the largest ocean on Earth?",
-        a: "Indian",
-        b: "Atlantic",
-        c: "Pacific",
-        d: "Arctic",
-        correct: "c",
+     {
+        "question":"What is the largest ocean on Earth?",
+        "option":["Indian","Atlantic","Pacific","Arctic"],
+        "answer":"Pacific"
     },
 
 ];
+
+quizbox = document.getElementById("quizbox");
+quizbox.innerHTML = ""
+questions.forEach((questions,index) => {
+    quizbox.innerHTML += `<div class="quizdiv">
+                <h4>Q: Did yo like this video?</h4>
+                <ul>
+                <li> <input type="radio" name="" id="">Option 1</li>
+                <li> <input type="radio" name="" id="">Option 1</li>
+                <li> <input type="radio" name="" id="">Option 1</li>
+                <li> <input type="radio" name="" id="">Option 1</li>
+                </ul>
+            </div>`;
+});
