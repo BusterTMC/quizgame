b74bc7b741026ace21a7f0ff15ea48a823dcf79d
BusterTMC <>
2024-02-01 15:19:39 +0000

    Added index and options

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 var questions = [
     {
         "question":"Who painted the Mona Lisa?",
-        "option":["Vincent van Gogh","Papblo Picasso","Leonardo da Vinci","Michelangelo"],
+        "options":["Vincent van Gogh","Papblo Picasso","Leonardo da Vinci","Michelangelo"],
         "answer":"Leonardo da Vinci"
     },
     {
         "question":"What is the capital of France?",
-        "option":["Paris","Monaco","Marseille","Nice"],
+        "options":["Paris","Monaco","Marseille","Nice"],
         "answer":"Paris"
     },
     {
         "question":"What is the tallest mountain in the world?",
-        "option":["K2","Mount Everest","Makalu","Lhotse"],
+        "options":["K2","Mount Everest","Makalu","Lhotse"],
         "answer":"Mount Everest"
     },
     {
         "question":"What is the largest planet in our solar system?",
-        "option":["Saturn","Uranus","Neptune","Jupiter"],
+        "options":["Saturn","Uranus","Neptune","Jupiter"],
         "answer":"Jupiter"
     },
     {
         "question":"Who wrote the play Romeo and Juliet?",
-        "option":["J.R.R. Tolkien","Charles Dickens","Lev Tolstoj","William Shakespeare"],
+        "options":["J.R.R. Tolkien","Charles Dickens","Lev Tolstoj","William Shakespeare"],
         "answer":"William Shakespeare"
     },
     {
         "question":"What is the capital city of Australia?",
-        "option":["Perth","Canberra","Melbourne","Sydney"],
+        "options":["Perth","Canberra","Melbourne","Sydney"],
         "answer":"Canberra"
     },
     {
         "question":"What is the primary language spoken in Brazil?",
-        "option":["French","Spanish","Portuguese","English"],
+        "options":["French","Spanish","Portuguese","English"],
         "answer":"Portuguese"
     },
      {
         "question":"Who is known as the inventor of the lightbulb?",
-        "option":["Thomas Edison","Nikola Tesla","Alexander Graham Bell","Albert Einstein"],
+        "options":["Thomas Edison","Nikola Tesla","Alexander Graham Bell","Albert Einstein"],
         "answer":"Thomas Edison"
     },
      {
         "question":"What is the largest ocean on Earth?",
-        "option":["Indian","Atlantic","Pacific","Arctic"],
+        "options":["Indian","Atlantic","Pacific","Arctic"],
         "answer":"Pacific"
     },
 
@@ -50,13 +50,14 @@ var questions = [
 quizbox = document.getElementById("quizbox");
 quizbox.innerHTML = ""
 questions.forEach((questions,index) => {
+    var options = ''
+    question.options.forEach((option,i) => {
+        options += ` <li><label><input type="radio" name="opt-${index}" id=""> ${option}<label></li>`
+    })
     quizbox.innerHTML += `<div class="quizdiv">
-                <h4>Q: Did yo like this video?</h4>
+                <h4>Q ${index+1} ${question.question}</h4>
                 <ul>
-                <li> <input type="radio" name="" id="">Option 1</li>
-                <li> <input type="radio" name="" id="">Option 1</li>
-                <li> <input type="radio" name="" id="">Option 1</li>
-                <li> <input type="radio" name="" id="">Option 1</li>
+                ${options}
                 </ul>
             </div>`;
 });
